b9fa7f6baa3eff6070e470e20ed2629b33f60580
Peter Jones <pjones@pmade.com>
2014-06-29 13:23:36 -0600

    Some documentation updates

--- README.md
+++ README.md
@@ -2,6 +2,42 @@
 
 A major mode for showing pomodoro timers.
 
+Eldoro works with tasks defined in an `org-mode` buffer.  When
+you start Eldoro from within an `org-mode` buffer it will gather
+all of headings which are children of the heading that point is
+currently on.  You can then start and stop pomodoro and break
+timers from within the Eldoro buffer.
+
+## Getting Started
+
+First, create an `org-mode` outline that looks something like
+this:
+
+~~~
+* Tasks I Want To Do
+** Write angry letter to Congress
+** Find a bug in OpenSSL
+** Upload compromising photos to FB
+~~~
+
+Now, move point to the first-level heading and start Eldoro with
+the `eldoro` interactive function.  Move point to a task you want
+to work on and press RET.
+
+To switch to a break, just press RET again.  If someone
+interrupts you, press i.
+
+![Screenshot](http://www.pmade.com/static/images/2014/a5fa5925980289be7d83d3e8dbe31e1c.png)
+
+## Reporting
+
+By default, Eldoro writes some basic statistics into `org-mode`
+properties.  If you want to compare the number of pomodori from
+day to day make sure you create new headings in the `org-mode`
+buffer every day.  Eldoro does not currently record timestamps
+with its statistics.  It would be nice if there was a better Org
+API for logging Eldoro statistics into a drawer.
+
 This mode runs the hook `eldoro-mode-hook`, as the final step
 during initialization.
 
@@ -19,5 +55,3 @@ r		eldoro-reset-statistics
 s		eldoro-stop-clock
 
 
-
-Totally needs documentation.
--- lisp/eldoro.el
+++ lisp/eldoro.el
@@ -1,7 +1,6 @@
-;;; -*- lexical-binding: t -*-
-;;; eldoro.el -- A pomodoro timer/tracker that works with org-mode.
+;;; eldoro.el -- A pomodoro timer/tracker that works with org-mode. -*- lexical-binding: t -*-
 
-;; Copyright (C) 2012-2013 Peter Jones <pjones@pmade.com>
+;; Copyright (C) 2012-2014 Peter Jones <pjones@pmade.com>
 ;;
 ;; Author: Peter Jones <pjones@pmade.com>
 ;; URL: https://github.com/pjones/eldoro
@@ -152,6 +151,7 @@ interruption counts."
   "Face for the active task in Eldoro."
   :group 'eldoro-faces)
 
+;;; Internal Variables.
 (defvar eldoro-buffer-name "*Eldoro*"
   "The name of the buffer used to show pomodoros.")
 
@@ -216,7 +216,43 @@ prompting."
     (error "Eldoro mode should be started from an org-mode heading"))))
 
 (define-derived-mode eldoro-mode fundamental-mode "Eldoro"
-  "A major mode for showing pomodoro timers."
+  "A major mode for showing pomodoro timers.
+
+Eldoro works with tasks defined in an `org-mode' buffer.  When
+you start Eldoro from within an `org-mode' buffer it will gather
+all of headings which are children of the heading that point is
+currently on.  You can then start and stop pomodoro and break
+timers from within the Eldoro buffer.
+
+## Getting Started
+
+First, create an `org-mode' outline that looks something like
+this:
+
+~~~
+* Tasks I Want To Do
+** Write angry letter to Congress
+** Find a bug in OpenSSL
+** Upload compromising photos to FB
+~~~
+
+Now, move point to the first-level heading and start Eldoro with
+the `eldoro' interactive function.  Move point to a task you want
+to work on and press RET.
+
+To switch to a break, just press RET again.  If someone
+interrupts you, press i.
+
+![Screenshot](http://www.pmade.com/static/images/2014/a5fa5925980289be7d83d3e8dbe31e1c.png)
+
+## Reporting
+
+By default, Eldoro writes some basic statistics into `org-mode'
+properties.  If you want to compare the number of pomodori from
+day to day make sure you create new headings in the `org-mode'
+buffer every day.  Eldoro does not currently record timestamps
+with its statistics.  It would be nice if there was a better Org
+API for logging Eldoro statistics into a drawer."
   :group 'eldoro
   (setq buffer-read-only t
         truncate-lines t)
